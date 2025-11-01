f07dc76ebf29dff324d7db87558bda9716fd102e
vtu30212 <vtu30212@veltech.edu.in>
2025-11-01 15:38:19 +0530

    Delete task 9_(d).py

--- task 9_(d).py
+++
@@ -1,9 +0,0 @@
-try:
-    initial_investment = float(input("Enter the initial investment amount: "))
-    final_investment = float(input("Enter the final investment amount: "))
-    roi = (final_investment - initial_investment) / initial_investment * 100
-    print("Return on Investment (ROI): {:.2f}".format(roi))
-except ValueError:
-    print("Error: Please enter a valid numeric value for the investment amount.")
-except ZeroDivisionError:
-    print("Error: Initial investment amount cannot be zero.")
