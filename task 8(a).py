cf1b5a99e937de36d82a0ba36612b7de544b0448
vtu30212 <vtu30212@veltech.edu.in>
2025-11-01 15:38:06 +0530

    Delete task 8(a).py

--- task 8(a).py
+++
@@ -1,9 +0,0 @@
-def fibonacci():
-    a, b = 0, 1
-    for _ in range(10):
-        yield a
-        a, b = b, a + b
-
-fibonacci_series = fibonacci()
-for number in fibonacci_series:
-    print(number, end=" ")
